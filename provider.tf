fe2e8f104d032ead5d550f214de9440acfcf974f
Nikola Suscevic <nikola.suscevic@gmail.com>
2025-10-08 16:31:36 +0200

    feat(networking): Add VPC

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+
+    organization = "TerraformWorks"
+
+    workspaces {
+      name = "terraform-vcs"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
