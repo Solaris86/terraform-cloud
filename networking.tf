c49458e750da5c540a63d45e7d3be0a4fdc1536a
Nikola Suscevic <nikola.suscevic@gmail.com>
2025-10-08 16:57:06 +0200

    feat(networking): Add Subnet

--- networking.tf
+++ networking.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "tf_cloud" {
     Name = "terraform-cloud"
   }
 }
+
+resource "aws_subnet" "tf_cloud" {
+  vpc_id     = aws_vpc.tf_cloud.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name = "terraform-cloud"
+  }
+}
